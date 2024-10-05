dbd78ea32379316ff08eb867248a979520eacb4c
Tonan <grooviepie@gmx.de>
2024-10-05 19:21:58 +0200

    machine implemented

--- entitys/player/player.gd
+++ entitys/player/player.gd
@@ -4,11 +4,10 @@ extends CharacterBody2D
 @onready var animation = $AnimationPlayer
 @onready var all_interactions = []
 @onready var interactLabel = $"Interaction components/InteractLabel"
+@onready var slush_machine = $"../slushMachine"
 
 var carrying_slime = false
-var blue_slime_inventory = 0
-var red_slime_inventory = 0
-var green_slime_inventory = 0
+var slimeColor = ""
 
 func _ready():
 	update_interactions()
@@ -21,6 +20,8 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("interact"):
 		execute_interaction()
+	if Input.is_action_just_pressed("interact2"):
+		execute_interaction2()
 	
 	if input_direction.x == 1:
 		$Sprite2D.flip_h = false
@@ -61,12 +62,55 @@ func execute_interaction():
 		match cur_interaction.interact_type:
 			"print_text" : print(cur_interaction.interact_value)
 			"blue_slime" : 
-				print(str(carrying_slime) + " " + str(blue_slime_inventory))
+				print(str(carrying_slime) + " " + str(slimeColor))
 				if carrying_slime == false:
 					print(cur_interaction.interact_value)
-					blue_slime_inventory += 1
+					slimeColor = "blue"
 					carrying_slime = true
 					cur_interaction.get_parent().queue_free()
-					print(str(carrying_slime) + " " + str(blue_slime_inventory))
-				
-				 
+					print(str(carrying_slime) + " " + str(slimeColor))
+			"slushMachine" : 
+				if carrying_slime == true:
+					print("Add Slime to Machine")
+					match slimeColor:
+						"blue" : slush_machine.blue_slime +=1
+						"red" : slush_machine.red_slime += 1
+						"green" : slush_machine.green_slime +=1
+					carrying_slime = false
+
+func execute_interaction2():
+	if all_interactions:
+		var cur_interaction = all_interactions[0]
+		match cur_interaction.interact_type:
+			"slushMachine":
+				if carrying_slime == false and slush_machine.isProducing == false and (slush_machine.blue_slime != 0 or slush_machine.red_slime !=0 or slush_machine.green_slime != 0):
+						startProduction()
+
+
+func startProduction():
+	slush_machine.isProducing = true
+	if slush_machine.blue_slime == 0 and slush_machine.red_slime == 0 and slush_machine.green_slime == 1:
+		runningProduction("green")
+	if slush_machine.blue_slime == 0 and slush_machine.red_slime == 1 and slush_machine.green_slime == 0:
+		runningProduction("red")
+	if slush_machine.blue_slime == 0 and slush_machine.red_slime == 1 and slush_machine.green_slime == 1:
+		runningProduction("yellow")
+	if slush_machine.blue_slime == 1 and slush_machine.red_slime == 0 and slush_machine.green_slime == 0:
+		runningProduction("blue") 
+	if slush_machine.blue_slime == 1 and slush_machine.red_slime == 0 and slush_machine.green_slime == 1:
+		runningProduction("cyan")
+	if slush_machine.blue_slime == 1 and slush_machine.red_slime == 1 and slush_machine.green_slime == 0:
+		runningProduction("purple")
+	if slush_machine.blue_slime == 1 and slush_machine.red_slime == 1 and slush_machine.green_slime == 1:
+		runningProduction("white")  
+	slush_machine.blue_slime = 0
+	slush_machine.red_slime = 0
+	slush_machine.green_slime = 0
+	
+	
+func runningProduction(color):
+	print(str(color)+"slushy")
+	#timer
+	#color
+	#finished true
+	#isproducing false
